dad8f63cc297f94e0024f0dcb796fa7dada5964e
Caius Durling <dev@caius.name>
2025-03-16 18:27:22 +0000

    Remove database schema/migrations from spec/dummy
    These are loaded from the parent engine during the test setup, so we don't need to copy them into place here to make it function.

--- spec/dummy/db/migrate/20250316173969_reset_counter_cache_on_panda_cms_template.panda_cms.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from panda_cms (originally 20240317163053)
-class ResetCounterCacheOnPandaCMSTemplate < ActiveRecord::Migration[7.1]
-  def change
-    Panda::CMS::Template.find_each { |t| Panda::CMS::Template.reset_counters(t.id, :pages) }
-  end
-end
--- spec/dummy/db/migrate/20250316173972_default_panda_cms_users_admin_to_false.panda_cms.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from panda_cms (originally 20240408084718)
-class DefaultPandaCMSUsersAdminToFalse < ActiveRecord::Migration[7.1]
-  def change
-    change_column :panda_cms_users, :admin, :boolean, default: false
-  end
-end
