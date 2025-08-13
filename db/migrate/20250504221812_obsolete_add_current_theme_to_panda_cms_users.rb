586620d089d4c05906329b0d768aff1d0dbb0984
James Inman <james@jamesinman.co.uk>
2025-08-13 09:47:12 +0100

    Fix duplicate migration name conflict
    Rename obsolete AddCurrentThemeToPandaCmsUsers migration
    to avoid conflict with panda-core migration

--- db/migrate/20250504221812_obsolete_add_current_theme_to_panda_cms_users.rb
+++ db/migrate/20250504221812_obsolete_add_current_theme_to_panda_cms_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddCurrentThemeToPandaCMSUsers < ActiveRecord::Migration[8.0]
+class ObsoleteAddCurrentThemeToPandaCmsUsers < ActiveRecord::Migration[8.0]
   def change
     # This migration is obsolete - users are now in panda_core_users table
     # and current_theme was added to the core users table
