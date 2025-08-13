4725c3cdb6bedbbfe1c0a88258412ef44956f30b
James Inman <james@jamesinman.co.uk>
2025-08-13 09:55:15 +0100

    Remove obsolete migration that conflicts with panda-core
    The AddCurrentThemeToPandaCmsUsers migration is no longer needed
    as users and their current_theme field are now handled by panda-core

--- db/migrate/20250504221812_obsolete_add_current_theme_to_panda_cms_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ObsoleteAddCurrentThemeToPandaCmsUsers < ActiveRecord::Migration[8.0]
-  def change
-    # This migration is obsolete - users are now in panda_core_users table
-    # and current_theme was added to the core users table
-  end
-end
