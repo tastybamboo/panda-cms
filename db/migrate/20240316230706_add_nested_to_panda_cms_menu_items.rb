1fd527d2c3ade0639918f2437e6023d51c440686
Caius Durling <dev@caius.name>
2025-11-17 21:15:12 +0000

    Stop using model classes in migration files
    These load the code as-is, which over time we can't guarantee works with the oldest migrations so its safer to avoid using model classes in migrations instead.
    
    Any new host application installing the engine will run all migrations from scratch

--- db/migrate/20240316230706_add_nested_to_panda_cms_menu_items.rb
+++ db/migrate/20240316230706_add_nested_to_panda_cms_menu_items.rb
@@ -11,16 +11,10 @@ class AddNestedToPandaCMSMenuItems < ActiveRecord::Migration[7.1]
     add_index :panda_cms_menu_items, :lft
     add_index :panda_cms_menu_items, :rgt
 
-    Panda::CMS::MenuItem.reset_column_information
-    Panda::CMS::MenuItem.rebuild!
-
     # Update pages whilst we're at it
     add_column :panda_cms_pages, :depth, :integer
     add_column :panda_cms_pages, :children_count, :integer, null: false, default: 0
     add_index :panda_cms_pages, :lft
     add_index :panda_cms_pages, :rgt
-
-    Panda::CMS::Page.reset_column_information
-    Panda::CMS::Page.rebuild!
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@ generator.create_templates
 generator.create_pages
 generator.create_menus
 Panda::CMS::Template.generate_missing_blocks
+
+if (homepage = Panda::CMS::Page.find_by(path: "/"))
+  main_menu = Panda::CMS::Menu.find_by(start_page_id: homepage.id, kind: :auto)
+  main_menu&.update(depth: 2)
+end
