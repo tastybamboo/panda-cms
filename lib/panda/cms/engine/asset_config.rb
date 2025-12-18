43f53b85f72638478a001e9a6ac78c85f3143d3a
James Inman <james@jamesinman.co.uk>
2025-12-18 21:28:46 +0000

    Remove asset pipeline duplication

--- lib/panda/cms/engine/asset_config.rb
+++ lib/panda/cms/engine/asset_config.rb
@@ -25,23 +25,6 @@ module Panda
               ]
             end
           end
-
-          # Add importmap paths from the engine
-          initializer "panda.cms.importmap", before: "importmap" do |app|
-            if app.config.respond_to?(:importmap)
-              # Create a new array if frozen
-              app.config.importmap.paths = app.config.importmap.paths.dup if app.config.importmap.paths.frozen?
-
-              # Add our paths
-              app.config.importmap.paths << root.join("config/importmap.rb")
-
-              # Handle cache sweepers similarly
-              if app.config.importmap.cache_sweepers.frozen?
-                app.config.importmap.cache_sweepers = app.config.importmap.cache_sweepers.dup
-              end
-              app.config.importmap.cache_sweepers << root.join("app/javascript")
-            end
-          end
         end
       end
     end
