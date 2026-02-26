a372d95843f5fd00d9e3dcca5ba771f7c7ab8aee
James Inman <james@jamesinman.co.uk>
2026-02-26 20:59:27 +0000

    Added test to form fields display_on_summary

--- db/migrate/20260226000001_add_display_on_summary_to_panda_cms_form_fields.rb
+++ db/migrate/20260226000001_add_display_on_summary_to_panda_cms_form_fields.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddDisplayOnSummaryToPandaCMSFormFields < ActiveRecord::Migration[7.2]
+class AddDisplayOnSummaryToPandaCMSFormFields < ActiveRecord::Migration[8.1]
   def change
     add_column :panda_cms_form_fields, :display_on_summary, :boolean, default: false, null: false
   end
